4933b89b8d43802455abe3deb2e813f594147995
Gavin Mogan <git@gavinmogan.com>
2020-04-20 13:45:15 -0700

    Fix url rewriter to include query strings (fixes tests <3)

--- src/main/java/io/jenkins/plugins/Filters.java
+++ src/main/java/io/jenkins/plugins/Filters.java
@@ -3,10 +3,13 @@ package io.jenkins.plugins;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.PreMatching;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.ext.Provider;
 import java.io.IOException;
-import java.net.URI;
+import java.util.List;
+import java.util.Map;
 
 @Provider
 @PreMatching
@@ -16,8 +19,15 @@ public class Filters implements ContainerRequestFilter {
     UriInfo uriInfo = reqContext.getUriInfo();
 
     // remove top level /api incase people use it directly
-    URI requestUri = uriInfo.getBaseUriBuilder().path( uriInfo.getPath().replaceFirst("^api/", "") ).build();
-
-    reqContext.setRequestUri( requestUri );
+    if (uriInfo.getPath().startsWith("api/")) {
+      MultivaluedMap<String, String> queryParametersMultiMap = uriInfo.getQueryParameters();
+      UriBuilder baseUriBuilder = uriInfo.getBaseUriBuilder();
+      baseUriBuilder.path(uriInfo.getPath().replaceFirst("^api/", ""));
+      baseUriBuilder.replaceQuery("");
+      for (Map.Entry<String, List<String>> queryEntry : queryParametersMultiMap.entrySet()) {
+        baseUriBuilder.queryParam(queryEntry.getKey(), queryEntry.getValue());
+      }
+      reqContext.setRequestUri(baseUriBuilder.build());
+    }
   }
 }
