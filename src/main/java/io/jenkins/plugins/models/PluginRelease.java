e40ed2edf4556a5fa87e0d76680da390e3404e73
Gavin Mogan <git@gavinmogan.com>
2020-06-01 21:56:32 -0700

    Process markdown on api side so we can get all the cool addons like issues and users and other githubie type things

--- src/main/java/io/jenkins/plugins/models/PluginRelease.java
+++ src/main/java/io/jenkins/plugins/models/PluginRelease.java
@@ -3,7 +3,16 @@ package io.jenkins.plugins.models;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.vladsch.flexmark.ext.gfm.issues.GfmIssuesExtension;
+import com.vladsch.flexmark.ext.gfm.strikethrough.StrikethroughExtension;
+import com.vladsch.flexmark.ext.gfm.tasklist.TaskListExtension;
+import com.vladsch.flexmark.ext.gfm.users.GfmUsersExtension;
+import com.vladsch.flexmark.ext.tables.TablesExtension;
+import com.vladsch.flexmark.html.HtmlRenderer;
+import com.vladsch.flexmark.parser.Parser;
+import com.vladsch.flexmark.util.data.MutableDataSet;
 
+import java.util.Arrays;
 import java.util.Date;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -12,6 +21,24 @@ public class PluginRelease {
   @JsonProperty("name") final private String name;
   @JsonProperty("published_at") final private Date publishedAt;
   @JsonProperty("body") private String body;
+
+  @JsonProperty("bodyHTML") private String getBodyHTML() {
+    MutableDataSet options = new MutableDataSet();
+
+    options.set(Parser.EXTENSIONS, Arrays.asList(
+      TablesExtension.create(),
+      StrikethroughExtension.create(),
+      GfmUsersExtension.create(),
+      TaskListExtension.create(),
+      GfmIssuesExtension.create()
+      ));
+
+    Parser parser = Parser.builder(options).build();
+    HtmlRenderer renderer = HtmlRenderer.builder(options).build();
+
+    return  renderer.render(parser.parse(this.body.replaceAll("<!--.*?-->", "")));
+  }
+
 
   @JsonCreator
   public PluginRelease(@JsonProperty("tag_name") String tagName, @JsonProperty("name") String name, @JsonProperty("published_at") Date publishedAt, @JsonProperty("body") String body) {
